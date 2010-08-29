470f973a29444e69593dd6c40303038ad8cc681f
Michael Pilgermann <kichkasch@gmx.de>
2010-08-29 16:11:37 +0200

    Basic device data model in place

--- device.py
+++ device.py
@@ -2,6 +2,15 @@
 #
 from google.appengine.ext import db
 
+class DeviceGroup(db.Model):
+    name = db.StringProperty()
+    
+class Manufactorer(db.Model):
+    name = db.StringProperty()
+    website = db.StringProperty()
+    
 class Device(db.Model):
     name = db.StringProperty()
     deviceID = db.StringProperty()
+    group = db.ReferenceProperty(DeviceGroup)
+    manufactorer = db.ReferenceProperty(Manufactorer)
--- driver.py
+++ driver.py
@@ -7,15 +7,23 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-from device import Device
+from device import Device, DeviceGroup, Manufactorer
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         device_query = Device.all().order('-name')
-        devices= device_query.fetch(10)
+        devices = device_query.fetch(10)
+        
+        groupQuery = DeviceGroup.all().order('-name')
+        groups = groupQuery.fetch(10)
+
+        manQuery = Manufactorer.all().order('-name')
+        mans = manQuery.fetch(10)
 
         template_values = {
+            'groups': groups, 
             'devices': devices,
+            'manufactorers': mans, 
             }
 
         path = os.path.join(os.path.dirname(__file__), 'index.html')
@@ -27,13 +35,34 @@ class AddDevice(webapp.RequestHandler):
         device = Device()
         device.name = self.request.get('deviceName')
         device.deviceID = self.request.get('deviceID')
+        query = DeviceGroup.all().filter('name = ', self.request.get('deviceGroup'))
+        device.group = query.fetch(1)[0]
+        query = Manufactorer.all().filter('name = ', self.request.get('manufactorer'))
+        device.manufactorer = query.fetch(1)[0]
         device.put()
+        self.redirect('/')
+
+class AddDeviceGroup(webapp.RequestHandler):
+    def post(self):
+        group = DeviceGroup()
+        group.name = self.request.get('groupName')
+        group.put()        
+        self.redirect('/')
+
+class AddManufactorer(webapp.RequestHandler):
+    def post(self):
+        man = Manufactorer()
+        man.name = self.request.get('manName')
+        man.website = self.request.get('manWebsite')
+        man.put()        
         self.redirect('/')
 
 def main():
     application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/newDevice', AddDevice)],
+                                      ('/newDevice', AddDevice), 
+                                      ('/newDeviceGroup', AddDeviceGroup), 
+                                      ('/newManufactorer', AddManufactorer)],
                                      debug=True)
     run_wsgi_app(application)
 
